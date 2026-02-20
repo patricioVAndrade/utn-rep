48811c5739f5166cb1e7d435ae4b24a47a986ee4
Patricio <patricioVandrade@gmail.com>
2026-02-20 01:39:19 -0300

    chore: clean gitignore, remove build artifacts and temp files

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_lENYh_O5.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_CTK4nMuo.mjs';
-export { renderers } from '../renderers.mjs';
