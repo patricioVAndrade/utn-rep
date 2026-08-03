6d2363a27a0bf4019e9354123d1f8c85caec3f5e
patricioVAndrade <patriciovandrade@gmail.com>
2026-08-03 13:11:42 -0300

    SEO implementado

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  site: 'https://utnrep.app',
+  integrations: [sitemap()],
   devToolbar: { enabled: false },
   vite: {
     plugins: [tailwindcss()],
